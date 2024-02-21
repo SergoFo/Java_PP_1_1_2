e4a29e5bb716f4cbcc47b050585477670dfb6c80
Nikita <108239130+NikitaNino@users.noreply.github.com>
2024-02-21 15:50:09 +0300

    в целом все ок (#1)
    Добавил твой коммит в ветку

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -11,18 +11,15 @@ public class Util {
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "111222";
 
-
-
     public static Connection getConnection() {
-        Connection connection = null;
         try {
             Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             System.out.println("Connection OK");
+            return connection;
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Connection ERROR");
             throw new RuntimeException(e);
         }
-        return connection;
     }
 }
